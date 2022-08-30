ff0853ed8ecbbe0c48b7631d9d4f94f5059f97c0
saeid <saeed221373@gmail.com>
2022-08-30 17:19:32 +0430

    init project

---
+++ Test Album/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Album
+//
+//  Created by saeid on 8/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
